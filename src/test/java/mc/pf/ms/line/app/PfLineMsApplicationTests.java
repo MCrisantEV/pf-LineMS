a90c3a1a7310cdb971a67929a8ec9c995581258f
MCrisantEV <56377073+MCrisantEV@users.noreply.github.com>
2019-11-08 15:57:12 -0500

    Update PfLineMsApplicationTests.java

--- src/test/java/mc/pf/ms/line/app/PfLineMsApplicationTests.java
+++ src/test/java/mc/pf/ms/line/app/PfLineMsApplicationTests.java
@@ -3,7 +3,7 @@ package mc.pf.ms.line.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PfLineMsApplicationTests {
 
 	@Test
